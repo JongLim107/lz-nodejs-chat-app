765d02b1deae697c1fd160918ae501be539bcbef
LZ <linzhong@Lins-MacBook-Pro.local>
2020-09-28 15:35:47 +0800

    Make server to support web sockets.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// Created server outside of the Express library we're creating it ourself
+const http = require('http')
+const server = http.createServer(app)
+
+// Create a new instance of socket.io to configure web sockets to work with our server.
+const socketio = require('socket.io')
+const io = socketio(server)
+
 // To serve static files such as images, CSS files, and JavaScript files
 // const publicDirectoryPath = path.join(__dirname, '../public')
 // app.use(express.static(publicDirectoryPath))
 app.use(express.static('public'))
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New WebSicket connection')
+})
+
+server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
